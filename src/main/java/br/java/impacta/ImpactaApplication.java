ddd7c85f04372660954dcde88a944367b7432093
KarenL19 <48663710+KarenL19@users.noreply.github.com>
2023-01-17 20:55:02 -0300

    Add metodos rest

---
+++ src/main/java/br/java/impacta/ImpactaApplication.java
@@ -0,0 +1,15 @@
+package br.java.impacta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpactaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpactaApplication.class, args);
+	}
+
+
+
+}
